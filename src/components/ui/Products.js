fd0a5353bceb8609a04e497c0ecc1a2cfddbf041
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-08-18 17:15:55 +0600

    mission accomplished 🔥🚀🔥

--- src/components/ui/Products.js
+++ src/components/ui/Products.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Card from "./Card";
 
 const Products = () => {
   return (
     <div className="px-12 lg:px-24 md:px-24 my-14">
       <h1 className="text-6xl font-bold text-center">Products</h1>
+      <div className="my-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 };
