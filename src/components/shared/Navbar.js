69955b3c1989a7e4637270c8c42264dc1cc451c2
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-08-20 21:20:13 +0600

    mission accomplished 🔥🚀🔥

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,3 +1,135 @@
+// import { useSession, signOut } from "next-auth/react";
+// import Link from "next/link";
+// import React from "react";
+
+// const Navbar = () => {
+//   const { data: session } = useSession();
+//   return (
+//     <div className="navbar lg:px-12 md:px-6 px-0 bg-base-300">
+//       <div className="navbar-start">
+//         <div className="dropdown">
+//           <label tabIndex={0} className="btn btn-ghost lg:hidden">
+//             <svg
+//               xmlns="http://www.w3.org/2000/svg"
+//               className="h-5 w-5"
+//               fill="none"
+//               viewBox="0 0 24 24"
+//               stroke="currentColor"
+//             >
+//               <path
+//                 strokeLinecap="round"
+//                 strokeLinejoin="round"
+//                 strokeWidth="2"
+//                 d="M4 6h16M4 12h8m-8 6h16"
+//               />
+//             </svg>
+//           </label>
+//           <ul
+//             tabIndex={0}
+//             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+//           >
+//             <Link href="/">
+//               <li>Home</li>
+//             </Link>
+//             <li>
+//               <details>
+//                 <summary>Categories</summary>
+//                 <ul className="p-2 lg:w-60 z-20">
+//                   <li>
+//                     <Link href={`/product/cpu_processor`}>CPU / Processor</Link>
+//                   </li>
+//                   <li>
+//                     <Link href={`/product/motherboard`}>Motherboard</Link>
+//                   </li>{" "}
+//                   <li>
+//                     <Link href={`/product/ram`}>RAM</Link>
+//                   </li>{" "}
+//                   <li>
+//                     <Link href={`/product/storage_device`}>Storage Device</Link>
+//                   </li>{" "}
+//                   <li>
+//                     <Link href={`/product/power_supply_unit`}>
+//                       Power Supply Unit
+//                     </Link>
+//                   </li>{" "}
+//                   <li>
+//                     <Link href={`/product/monitor`}>Monitor</Link>
+//                   </li>
+//                 </ul>
+//               </details>
+//             </li>
+//             <Link href="/blog">
+//               <li>Item 3</li>
+//             </Link>
+//           </ul>
+//         </div>
+//         <Link href="/">
+//           <li className="btn btn-ghost text-primary font-bold normal-case lg:text-5xl md:text-3xl text-xl">
+//             Tech
+//           </li>
+//         </Link>
+//       </div>
+//       <div className="navbar-center hidden lg:flex">
+//         <ul className="menu menu-horizontal px-1">
+//           <li>
+//             <Link href="/">Home</Link>
+//           </li>
+//           <li>
+//             <details>
+//               <summary>Categories</summary>
+//               <ul className="p-2 lg:w-60 z-20">
+//                 <li>
+//                   <Link href={`/product/cpu_processor`}>CPU / Processor</Link>
+//                 </li>
+//                 <li>
+//                   <Link href={`/product/motherboard`}>Motherboard</Link>
+//                 </li>{" "}
+//                 <li>
+//                   <Link href={`/product/ram`}>RAM</Link>
+//                 </li>{" "}
+//                 <li>
+//                   <Link href={`/product/storage_device`}>Storage Device</Link>
+//                 </li>{" "}
+//                 <li>
+//                   <Link href={`/product/power_supply_unit`}>
+//                     Power Supply Unit
+//                   </Link>
+//                 </li>{" "}
+//                 <li>
+//                   <Link href={`/product/monitor`}>Monitor</Link>
+//                 </li>
+//               </ul>
+//             </details>
+//           </li>
+//           <li>
+//             <Link href="/blog">
+//               <li>Blog</li>
+//             </Link>
+//           </li>
+//         </ul>
+//       </div>
+//       <div className="navbar-end">
+//         <li className="btn btn-primary">PC BUILDER</li>
+//         {session?.user ? (
+//           <li
+//             onClick={() => signOut()}
+//             className="btn btn-ghost text-primary hover:bg-transparent focus:bg-transparent"
+//           >
+//             Logout
+//           </li>
+//         ) : (
+//           <Link href="/login">
+//             <li className="btn btn-ghost text-primary hover:bg-transparent focus:bg-transparent">
+//               Login
+//             </li>
+//           </Link>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Navbar;
 import { useSession, signOut } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
@@ -28,9 +160,9 @@ const Navbar = () => {
             tabIndex={0}
             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
           >
-            <Link href="/">
-              <li>Home</li>
-            </Link>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
             <li>
               <details>
                 <summary>Categories</summary>
@@ -38,36 +170,18 @@ const Navbar = () => {
                   <li>
                     <Link href={`/product/cpu_processor`}>CPU / Processor</Link>
                   </li>
-                  <li>
-                    <Link href={`/product/motherboard`}>Motherboard</Link>
-                  </li>{" "}
-                  <li>
-                    <Link href={`/product/ram`}>RAM</Link>
-                  </li>{" "}
-                  <li>
-                    <Link href={`/product/storage_device`}>Storage Device</Link>
-                  </li>{" "}
-                  <li>
-                    <Link href={`/product/power_supply_unit`}>
-                      Power Supply Unit
-                    </Link>
-                  </li>{" "}
-                  <li>
-                    <Link href={`/product/monitor`}>Monitor</Link>
-                  </li>
+                  {/* Other category links */}
                 </ul>
               </details>
             </li>
-            <Link href="/blog">
-              <li>Item 3</li>
-            </Link>
+            <li>
+              <Link href="/blog">Blog</Link>
+            </li>
           </ul>
         </div>
-        <Link href="/">
-          <li className="btn btn-ghost text-primary font-bold normal-case lg:text-5xl md:text-3xl text-xl">
-            Tech
-          </li>
-        </Link>
+        <li className="btn btn-ghost text-primary font-bold normal-case lg:text-5xl md:text-3xl text-xl">
+          <Link href="/">Tech</Link>
+        </li>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
@@ -77,39 +191,16 @@ const Navbar = () => {
           <li>
             <details>
               <summary>Categories</summary>
-              <ul className="p-2 lg:w-60 z-20">
-                <li>
-                  <Link href={`/product/cpu_processor`}>CPU / Processor</Link>
-                </li>
-                <li>
-                  <Link href={`/product/motherboard`}>Motherboard</Link>
-                </li>{" "}
-                <li>
-                  <Link href={`/product/ram`}>RAM</Link>
-                </li>{" "}
-                <li>
-                  <Link href={`/product/storage_device`}>Storage Device</Link>
-                </li>{" "}
-                <li>
-                  <Link href={`/product/power_supply_unit`}>
-                    Power Supply Unit
-                  </Link>
-                </li>{" "}
-                <li>
-                  <Link href={`/product/monitor`}>Monitor</Link>
-                </li>
-              </ul>
+              <ul className="p-2 lg:w-60 z-20">{/* Other category links */}</ul>
             </details>
           </li>
           <li>
-            <Link href="/blog">
-              <li>Blog</li>
-            </Link>
+            <Link href="/blog">Blog</Link>
           </li>
         </ul>
       </div>
       <div className="navbar-end">
-        <a className="btn btn-primary">PC BUILDER</a>
+        <li className="btn btn-primary">PC BUILDER</li>
         {session?.user ? (
           <li
             onClick={() => signOut()}
@@ -118,11 +209,9 @@ const Navbar = () => {
             Logout
           </li>
         ) : (
-          <Link href="/login">
-            <li className="btn btn-ghost text-primary hover:bg-transparent focus:bg-transparent">
-              Login
-            </li>
-          </Link>
+          <li className="btn btn-ghost text-primary hover:bg-transparent focus:bg-transparent">
+            <Link href="/login">Login</Link>
+          </li>
         )}
       </div>
     </div>
--- src/components/ui/Banner.js
+++ src/components/ui/Banner.js
@@ -11,12 +11,17 @@ const Banner = () => {
     >
       <div className="hero-overlay bg-opacity-80"></div>
       <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-md my-20 lg:my-40 md:my-40">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+        <div className=" my-20 lg:my-40 md:my-40">
+          <h1 className="mb-5 text-5xl font-bold">
+            🔥Discover Your Ultimate PC Experience with Us!🔥
+          </h1>
           <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            Welcome to the world of limitless possibilities in PC building.
+            Unleash your creativity, power your passion, and elevate your
+            gaming, productivity, and entertainment with our curated selection
+            of top-notch components and accessories. Whether you're a seasoned
+            enthusiast or a first-time builder, our PC building website is here
+            to guide you every step of the way.
           </p>
           <button className="btn btn-primary">Get Started</button>
         </div>
