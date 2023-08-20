3df5f341a6cb7f2ee5b05338bd5a1f19e8d99048
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-08-20 21:40:01 +0600

    mission accomplished 🔥🚀🔥

---
+++ src/pages/product/[category]/[category].js
@@ -0,0 +1,41 @@
+import FeaturedCard from "@/components/FeaturedCard";
+import { useRouter } from "next/router";
+import React from "react";
+
+const ProductCategory = ({ products, params }) => {
+  const router = useRouter();
+  return (
+    <div className="pb-20">
+      <FeaturedCard
+        products={products}
+        title="Products"
+        categoryName={router.query?.category}
+      />
+    </div>
+  );
+};
+
+export default ProductCategory;
+
+export const getStaticPaths = async () => {
+  const res = await fetch("https://pc-builder-json.vercel.app/api/categories");
+  const categories = await res.json();
+  const paths = categories.map((category) => ({
+    params: { category },
+  }));
+
+  return { paths, fallback: false };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const res = await fetch(
+    `https://pc-builder-json.vercel.app/products?category=${params.category}`
+  );
+  const products = await res.json();
+
+  return {
+    props: {
+      products,
+    },
+  };
+};
